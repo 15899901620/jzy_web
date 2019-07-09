953c869a8cb48ad8b06ba83292a88a575439337d
qijun <1506728838@qq.com>
2019-07-09 15:27:07 +0800

    11

--- pages/special.vue
+++ pages/special.vue
@@ -8,12 +8,11 @@
          <div class="TitleName" style="border-left: 3px solid #279eff;">产品列表</div>
        </div>
 
-       <div class="indexXhTitle   dflexAlem graybg">
+       <div  class="indexXhTitle   dflexAlem graybg">
          <span style="width: 7%;">合同</span>
-         <span style="width: 7%;">品种</span>
-         <span style="width: 7%;">牌号</span>
+         <span style="width: 8%;">编号</span>
+         <span style="width: 8%;">商品名称</span>
          <span style="width: 8%;">厂商</span>
-         <span style="width: 9%;">交货地</span>
          <span style="width: 9%;">交货时间</span>
          <span style="width: 9%;">单价（元/吨）</span>
          <span style="width: 9%;">会同数量</span>
@@ -23,64 +22,20 @@
          <span style="width: 11%;">操作</span>
        </div>
 
-       <ul class="indexXhlist">
-
-         <li>
+       <ul class="indexXhlist"  >
+         <li v-for="(special,i) in items">
            <span class="dflexAlemJust" style="width: 7%;"><div class="contact contactblue">预</div></span>
-           <span style="width: 7%;">PP</span>
-           <span style="width: 7%;">F08</span>
-           <span style="width: 8%;">巨正源</span>
-           <span style="width: 9%;">东莞</span>
+           <span style="width: 8%;">{{special.skuNo}}</span>
+           <span style="width: 8%;">{{special.skuName}}</span>
+           <span style="width: 8%;">{{special.manufacturer}}</span>
            <span style="width: 9%;">4月中旬</span>
-           <span class="orangeFont" style="width: 9%;">¥10550</span>
-           <span style="width: 9%;">100.000</span>
+           <span     class="orangeFont"  style="width: 9%;">{{special.finalPrice}}/{{special.uomName}}</span>
+           <span style="width: 9%;">{{special.availableNum}}</span>
            <span style="width: 9%;">20.000</span>
            <span style="width: 9%;">50.000</span>
            <span  class="dflex" style="width: 13%; align-items: center;">
 							<div class="NumReduice" style="margin: 0; width: 55%; margin-left: 25px;">
-			        			<span class="gray" style="width: 25%;">-</span>
-			        			<input class="TextNum" type="text" name="" id="" value="">
-			        			<span class="gray"  style="width: 25%;">+</span>
-		        			</div>
-		        			<div class="Numtip"></div>
-						</span>
-           <span style="width: 11%;"><div class="ListBtn">下单</div></span>
-         </li>
-         <li>
-           <span class="dflexAlemJust" style="width: 7%;"><div class="contact contactblue">预</div></span>
-           <span style="width: 7%;">PP</span>
-           <span style="width: 7%;">F08</span>
-           <span style="width: 8%;">巨正源</span>
-           <span style="width: 9%;">东莞</span>
-           <span style="width: 9%;">4月中旬</span>
-           <span class="orangeFont" style="width: 9%;">¥10550</span>
-           <span style="width: 9%;">100.000</span>
-           <span style="width: 9%;">20.000</span>
-           <span style="width: 9%;">50.000</span>
-           <span  class="dflex" style="width: 13%; align-items: center;">
-							<div class="NumReduice" style="margin: 0; width: 55%; margin-left: 25px;">
-			        			<span class="gray" style="width: 25%;">-</span>
-			        			<input class="TextNum" type="text" name="" id="" value="">
-			        			<span class="gray"  style="width: 25%;">+</span>
-		        			</div>
-		        			<div class="Numtip"></div>
-						</span>
-           <span style="width: 11%;"><div class="ListBtn">下单</div></span>
-         </li>
-         <li>
-           <span class="dflexAlemJust" style="width: 7%;"><div class="contact contactblue">预</div></span>
-           <span style="width: 7%;">PP</span>
-           <span style="width: 7%;">F08</span>
-           <span style="width: 8%;">巨正源</span>
-           <span style="width: 9%;">东莞</span>
-           <span style="width: 9%;">4月中旬</span>
-           <span class="orangeFont" style="width: 9%;">¥10550</span>
-           <span style="width: 9%;">100.000</span>
-           <span style="width: 9%;">20.000</span>
-           <span style="width: 9%;">50.000</span>
-           <span  class="dflex" style="width: 13%; align-items: center;">
-							<div class="NumReduice" style="margin: 0; width: 55%; margin-left: 25px;">
-			        			<span class="gray" style="width: 25%;">-</span>
+                            		        			<span class="gray" style="width: 25%;">-</span>
 			        			<input class="TextNum" type="text" name="" id="" value="">
 			        			<span class="gray"  style="width: 25%;">+</span>
 		        			</div>
@@ -89,9 +44,9 @@
            <span style="width: 11%;"><div class="ListBtn">下单</div></span>
          </li>
        </ul>
-       <div class="whitebg ovh">
-         <Pagination></Pagination>
-       </div>
+         <div class="whitebg ovh text-xs-center" style="padding: 30px 0">
+             <Page :total="this.total"  @on-change="changePage" :page-size="page_size"  :current="current_page" show-total show-elevator   />
+         </div>
      </div>
 
    </div>
@@ -99,13 +54,49 @@
 
 <script>
   import Pagination from '../components/pagination/pagination'
+  import { adposition,specialList } from '../api/special'
   export default {
         name: "special",
     components:{
       Pagination,
+    },
+      data () {
+      return {
+          current_page: !this.$route.query.page ? 1 : this.$route.query.page,
+          total_page: (this.total/this.page_size) < 1 ? 1 : parseInt(this.total/this.page_size),
+          page_size:10,
+          total:0,
+          items:[]
+      }
+  },
+  methods:{
+      async specialData(){
+          let params = {
+              current_page: this.current_page,
+              page_size: this.page_size,
+              status: 'CO'
+          }
+          const res = await specialList(this, params)
 
-    }
-    }
+          this.items = res.data.items
+          console.log(this.items)
+          this.total = res.data.total
+          console.log(this.items)
+
+      },
+      changePage (row) {
+          this.$router.push({name:'special',query:{page:row}})
+          this.current_page = row
+          this.specialData()
+      }
+  },
+  created () {
+
+  },
+  mounted() {
+      this.specialData()
+  }
+  }
 </script>
 
 <style scoped>
