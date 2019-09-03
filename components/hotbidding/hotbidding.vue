5a8de73ef9c4b388aac94517db3e574a2429b664
mingyu17173 <fgf67@163.com>
2019-09-03 15:12:40 +0800

    1

--- components/hotbidding/hotbidding.vue
+++ components/hotbidding/hotbidding.vue
@@ -76,6 +76,7 @@ export default {
           //   isActive:''
           // }
           // let res = await auctionPage(this,params)
+          console.log(this.hotbidderList)
           if(this.hotbidderList){
             this.indexAuction= this.hotbidderList
             var arrayData=[]
