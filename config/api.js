8c085501618247736d2bc3964429e3162e14f2d4
open818 <270608543@qq.com>
2019-10-11 11:15:38 +0800

    no message

--- config/api.js
+++ config/api.js
@@ -126,7 +126,6 @@ let api = {
     priceListInfo:'/bill/priceList/getPrice', //查询物料价目信息
     NewPrice:'/bill/auctionBid/getCurrBid',   //获取最新的竞拍价
     auctionRecord:'/bill/auctionBid/page',    //获取竞拍记录
-    auctionMineRecord:'/bill/auctionBid/list',    //获取竞拍我的出价记录
     AddauctionPrice:'/bill/auctionBid',    //添加我的出价记录
     gainauctionrecord:'/bill/auctionBid/getMyBid',  //获取出价记录
     AddBondRecord:'/bill/auctionDeposit',           //添加保证金记录
