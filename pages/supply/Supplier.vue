4de175918dc857b4c71d1086c78aaa46fac3f23a
qijun <1506728838@qq.com>
2019-10-18 14:39:31 +0800

    111

--- pages/supply/Supplier.vue
+++ pages/supply/Supplier.vue
@@ -53,11 +53,12 @@
 				<div class="check mt5 blackFont" style="margin-left:15px;" v-if='item.status==1 && item.isQuote==0' >未报价</div>
 				<div class="check mt5 blackFont" style="margin-left:15px;" v-if='item.status==1 && item.isQuote== 1'>竞价中</div>
 				<div class="check mt5 blackFont" style="margin-left:15px;" v-if='item.status==2 && item.isWin == 0'>未中标</div>
-				<div class="check mt5 blackFont" style="margin-left:15px;" v-if='item.status==2 &&  item.isWin == 0'>已中标 </div>
+				<div class="check mt5 blackFont" style="margin-left:15px;background-color: #f13a39" v-if='item.status==2 &&  item.isWin == 0'>恭喜中标 </div>
               </td>
               <td class="operate" style="width: 10%;">
                 <div class="check mt5 blackFont" style="margin-left:15px;cursor: pointer;"  v-if='item.status==1 && item.isQuote==0' @click="oldtime(item)">我要报价</div>
-				<div class="check mt5 blackFont" style="margin-left:15px;cursor: pointer;"  @click="detailLog(item)">查看详情</div>
+				<div class="check mt5 blackFont" style="margin-left:15px;background-color: gainsboro;" v-else-if='item.status==0'  >查看详情</div>
+				<div class="check mt5 blackFont" style="margin-left:15px;cursor: pointer;" v-else  @click="detailLog(item)">查看详情</div>
               </td>
             </tr>
             </tbody>
