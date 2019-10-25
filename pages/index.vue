6aa588e54749abe09b72acbcba453d487dceb1dd
mingyu17173 <fgf67@163.com>
2019-10-25 10:36:36 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -149,9 +149,6 @@ export default {
       articleCates:{},
     }
   },
-  created(){
-    console.log(process.env.NODE_ENV)
-  },
   computed: {
     ...mapState({
       bannerinfo: state => state.system.bannerinfo
