93678058cdbcb3b449fdb79905cfe8a925d54c55
open818 <270608543@qq.com>
2020-02-06 10:12:39 +0800

    no message

--- pages/bidders/_page.vue
+++ pages/bidders/_page.vue
@@ -7,7 +7,7 @@
           <!-- <img :src="item.adImg"/>-->
         </div>
       </template>
-<!--      我的竞拍列表-->
+      <!--      我的竞拍列表-->
       <div class="w1200" style="margin-top: 20px">
         <div class="titlelist" v-if="partakeList.length > 0">
           我的竞拍
@@ -112,7 +112,8 @@
                     <div class="statusicon startauction">正在竞拍</div>
                     <div class="ml20"><span>距离结束 ：</span>
                       <span class="fs18">
-                        <TimeDown :currTime="items.currTime" :timeStyleType='2' :endTime="items.realEndTime" endMsg="已结束"
+                        <TimeDown :currTime="items.currTime" :timeStyleType='2' :endTime="items.realEndTime"
+                                  endMsg="已结束"
                                   :onTimeOver="reloadPage"></TimeDown>
                       </span>
                     </div>
@@ -134,20 +135,30 @@
                     </div>
                   </template>
 
-                  <div class="ml50"><span class="gray">竞拍时长：</span>{{$utils.timeBetween(items.beginTime, items.realEndTime)}}</div>
-                  <div class="cancel_follow " v-if="items.statusType != '3' && (items.isFollow ? 1 : 0) && $store.state.memberToken " @click="Bidersdelete(items,index)">已关注</div>
-                  <div class="follow" v-else-if="items.statusType != '3' && (items.isFollow ? 0 : 1) && $store.state.memberToken" @click="BidersAdd(items,index)">关注</div>
-                 </div>
+                  <div class="ml50"><span class="gray">竞拍时长：</span>{{$utils.timeBetween(items.beginTime,
+                    items.realEndTime)}}
+                  </div>
+                  <div class="cancel_follow "
+                       v-if="items.statusType != '3' && (items.isFollow ? 1 : 0) && $store.state.memberToken "
+                       @click="Bidersdelete(items,index)">已关注
+                  </div>
+                  <div class="follow"
+                       v-else-if="items.statusType != '3' && (items.isFollow ? 0 : 1) && $store.state.memberToken"
+                       @click="BidersAdd(items,index)">关注
+                  </div>
+                </div>
 
                 <div class="acuProduct " style="margin-top: 53px; margin-bottom: 30px;">
                   <span class="fs20" style="position: relative;margin-top: 15px">{{items.skuName}}
-                    <i v-if="items.is_jry" style="width: 15px; height: 18px; position: absolute; top: -6px;   background:url('/img/Yi_icon.png')no-repeat;"></i></span>
+                    <i v-if="items.is_jry"
+                       style="width: 15px; height: 18px; position: absolute; top: -6px;   background:url('/img/Yi_icon.png')no-repeat;"></i></span>
                   <div class="mt10 fs14 dflex">
                     <div class="btmunv"><span class="iv_title">起拍价</span> ：<span class="orangeFont fwb fs16">{{items.finalPriceFormat}}</span>
                     </div>
                     <div class="fs14 dflex" style="align-items: center;position: relative">
                       <span class="iv_title">竞拍数量</span> ：<span class="orangeFont fs16">{{items.totalNum}}</span>{{items.uomName}}
-                                 <i :title="`限购${items.limitNum}`" v-if="items.limitNum > 0" style="width: 15px; height: 18px; position: absolute; top: -10px; right: -22px; background:url('/img/Xian_icon.png')no-repeat;"></i>
+                      <i :title="`限购${items.limitNum}`" v-if="items.limitNum > 0"
+                         style="width: 15px; height: 18px; position: absolute; top: -10px; right: -22px; background:url('/img/Xian_icon.png')no-repeat;"></i>
                       <template v-if="items.isAll == 1">
                         <span class="orangeFont fs16 ml30">整单</span>
                       </template>
@@ -191,7 +202,8 @@
             </p>
             <div class="text-xs-center" style="padding: 18px 0; text-align: center;">
               <pages :total="auctionTotal" :show-total="showTotal" :value="current_page"
-                     :pageSize="page_size" :otherParams="`statusType=${this.statusType}&planType=${this.planType}`"></pages>
+                     :pageSize="page_size"
+                     :otherParams="`statusType=${this.statusType}&planType=${this.planType}`"></pages>
             </div>
           </div>
 
@@ -228,213 +240,219 @@
 </template>
 
 <script>
-	import {mapState} from 'vuex'
-	import Header from '../../components/header'
-	import Footer from '../../components/footer'
-	import pagination from '../../components/pagination'
-	import TimeDown from '../../components/timeDown'
-	import server from "../../config/api";
-	import {sendCurl} from '../../api/common'
+import {mapState} from 'vuex'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import pagination from '../../components/pagination'
+import TimeDown from '../../components/timeDown'
+import server from "../../config/api";
+import {sendCurl} from '../../api/common'
 
 
-	export default {
-		name: 'bidders',
-		components: {
-			Header,
-			Footer,
-			pages: pagination.pages,
-			TimeDown
-		},
+export default {
+	name: 'bidders',
+	components: {
+		Header,
+		Footer,
+		pages: pagination.pages,
+		TimeDown
+	},
 
-		data() {
-			return {
-				CurrSelect: 0,
-				current_page: parseInt(this.$route.query.page) || 1,
+	data() {
+		return {
+			CurrSelect: 0,
+			current_page: parseInt(this.$route.query.page) || 1,
+			page_size: 10,
+			page: 1,
+			NowTime: '',
+			Auctionlist: '',
+			AuctionTip: '暂无竞拍活动',
+			tabMain: ['1', '2', '3'],
+			status: 2,
+			index: 0,
+			isFollow: 0,
+			aclist: '',
+
+			statusType: this.$route.query.statusType || 0,
+			planType: this.$route.query.planType || 0
+		}
+	},
+	fetch({store, params, query}) {
+		return Promise.all([
+			// 获取顶部、中部、底部导航信息
+			store.dispatch('common/getNavList'),
+			// 获取系统配置
+			store.dispatch('common/getSysConfig'),
+			// 获取友情链接
+			store.dispatch('common/getFriendlyList'),
+			// 获取轮播图
+			store.dispatch('common/getBannerList', 4),
+			// 获取底部帮助分类
+			store.dispatch('helper/getHelpCate', {catId: 0, indexShow: 1}),
+			store.dispatch('article/getindexArticleList', {catId: 8}),
+			// 获取竞拍列表
+			store.dispatch('bidders/getAuctionList', {
+				current_page: query.page || 1,
 				page_size: 10,
-				page: 1,
-				NowTime: '',
-				Auctionlist: '',
-				AuctionTip: '暂无竞拍活动',
-				tabMain: ['1', '2', '3'],
-				status: 2,
-				index: 0,
-				isFollow: 0,
-				aclist: '',
-
-        statusType: this.$route.query.statusType || 0,
-        planType: this.$route.query.planType || 0
-			}
-		},
-		fetch({store, params, query}) {
-			return Promise.all([
-				// 获取顶部、中部、底部导航信息
-				store.dispatch('common/getNavList'),
-				// 获取系统配置
-				store.dispatch('common/getSysConfig'),
-				// 获取友情链接
-				store.dispatch('common/getFriendlyList'),
-				// 获取轮播图
-				store.dispatch('common/getBannerList', 4),
-				// 获取底部帮助分类
-				store.dispatch('helper/getHelpCate', {catId: 0, indexShow: 1}),
-				store.dispatch('article/getindexArticleList', {catId: 8}),
-				// 获取竞拍列表
-				store.dispatch('bidders/getAuctionList', {current_page: query.page || 1, page_size: 10, status_type: query.statusType,plan_type: query.planType,product_type:1}),
-				// 网站公告
-				store.dispatch('article/getNoticeList', {typeId: 4, current_page: 1, page_size: 15, sortBy:'add_time', desc:'1'}),
-				// 获取用户参与列表
-				store.dispatch('bidders/getPartakeList'),
-				// 侧边广告栏
-				store.dispatch('system/getBannerInfo', {
-					positionId: 6
-				}),
-			])
-		},
-		created() {
-			this.SourceData()
-		},
-		mounted() {
-		},
-		computed: {
-			...mapState({
-				auctionTotal: state => state.bidders.auctionTotal,
-				auctionList: state => state.bidders.auctionList,
-
-				biddersbeingData: state => state.bidders.biddersbeingData,
-				bidderssoonData: state => state.bidders.bidderssoonData,
-				biddersendData: state => state.bidders.biddersendData,
-				bannerinfo: state => state.system.bannerinfo,      // 页面banner
-				noticeList: state => state.article.noticeList,     // 侧边竞拍广告
-
-				partakeList: state => state.bidders.partakeList,  // 我的竞拍
-				sideadvImg: state => state.system.bannerinfo,    // 侧边广告栏
+				status_type: query.statusType,
+				plan_type: query.planType,
+				product_type: 1
 			}),
-			statusTypeName: function(){
-				let type = this.statusType
-				if(type == '0'){
-          return '全部竞拍'
-				}else if (type == '1'){
-					return '正在竞拍'
-				}else if (type == '2'){
-					return '即将开始'
-				}else if (type == '3'){
-					return '竞拍结束'
-				}
-      },
-			planTypeName: function(){
-				let type = this.planType
-				if(type == '0'){
-					return '我的竞拍'
-				}else if(type == '1'){
-					return '已中标'
-				}else if(type == '2') {
-					return '未中标'
-				}
+			// 网站公告
+			store.dispatch('article/getNoticeList', {
+				typeId: 4,
+				current_page: 1,
+				page_size: 15,
+				sortBy: 'add_time',
+				desc: '1'
+			}),
+			// 获取用户参与列表
+			store.dispatch('bidders/getPartakeList'),
+			// 侧边广告栏
+			store.dispatch('system/getBannerInfo', {
+				positionId: 6
+			}),
+		])
+	},
+	created() {
+		this.SourceData()
+	},
+	mounted() {
+	},
+	computed: {
+		...mapState({
+			auctionTotal: state => state.bidders.auctionTotal,
+			auctionList: state => state.bidders.auctionList,
+
+			biddersbeingData: state => state.bidders.biddersbeingData,
+			bidderssoonData: state => state.bidders.bidderssoonData,
+			biddersendData: state => state.bidders.biddersendData,
+			bannerinfo: state => state.system.bannerinfo,      // 页面banner
+			noticeList: state => state.article.noticeList,     // 侧边竞拍广告
+
+			partakeList: state => state.bidders.partakeList,  // 我的竞拍
+			sideadvImg: state => state.system.bannerinfo,    // 侧边广告栏
+		}),
+		statusTypeName: function () {
+			let type = this.statusType
+			if (type == '0') {
+				return '全部竞拍'
+			} else if (type == '1') {
+				return '正在竞拍'
+			} else if (type == '2') {
+				return '即将开始'
+			} else if (type == '3') {
+				return '竞拍结束'
 			}
 		},
-		methods: {
-      Tospot(link){
-        if(link){
-          this.$router.push({name:link})
-        }
-      },
-      showTotal(total) {
-          return `全部 ${total} 条`;
-      },
-      reloadPage() {
-          location.reload()
-      },
-			getIcon(items){
-      	if(this.$store.state.memberToken) {
-      		if(items.statusType == '1' || items.statusType == '2'){
-						if(items.depositNum > 0){
-							return 'auction_over_01'
-            }
-          }else{
-						if(items.totalPlanNum > 0){
-							return 'auction_over_02'
-						}else{
-							if(items.depositNum > 0){
-								return 'auction_over_03'
-							}
-            }
-          }
-				}
-      },
-          handleStatusTypeOption(type){
-              this.statusType = type
-              location.href = '/bidders?page='+this.current_page+'&statusType='+this.statusType+"&planType="+this.planType
-      },
-          handlePlanTypeOption(type){
-				this.planType = type
-				location.href = '/bidders?page='+this.current_page+'&statusType='+this.statusType+"&planType="+this.planType
-          },
-
-      async BidersAdd(items,index){
-         let params={
-          auctionIds:items.id
-        }
-        const res = await sendCurl(this,server.api.Auction.getfollow,params,false)
-         if(!res.data.errorCode && res.data){
-             this.$store.dispatch('bidders/getAuctionList', {current_page: this.current_page || 1, page_size: 6,product_type:1})
-         }
-
-      },
-      async Bidersdelete(items,index){
-         let params={
-          auctionId:items.id
-        }
-        const res = await sendCurl(this,server.api.Auction.cancelfollow,params,false)
-         if(!res.data.errorCode && res.data){
-             this.$store.dispatch('bidders/getAuctionList', {current_page: this.current_page || 1, page_size: 6,product_type:1})
-         }
-
-      },
-      async BidersCancel(items,index){
-        let params={
-          auctionId:items.id
-        }
-        const res = await sendCurl(this,server.api.Auction.cancelfollow,params,false)
-        if(!res.data.errorCode && res.data){
-          this.$store.dispatch('bidders/getAuctionList', {current_page: this.current_page || 1, page_size: 6,product_type:1})
-        }else{
-          this.$Notice.warning({
-            title: res.message,
-           });
-        }
-
-      },
-      async SourceData() {
-        let params={
-          catId:8
-        }
-         const res = await sendCurl(this,server.api.information.getArticleList,params,false)
-        this.aclist = res.data.items
-      },
-			//跳转详情页
-			BidersDetail(id) {
-
-				if (this.$store.state.memberToken) {
-					location.href = '/bidders/detail/' + id
-				} else {
-					this.$Modal.confirm({
-						title: '提示',
-						content: '<p>请登录后参与竞拍！</p>',
-						okText: '去登录',
-						onOk: () => {
-							location.href = '/login'
-						}
-					});
-				}
-
-			}
-		},
-		watch: {
-			'$route'(to, from) {
-				this.$router.go(0);
+		planTypeName: function () {
+			let type = this.planType
+			if (type == '0') {
+				return '我的竞拍'
+			} else if (type == '1') {
+				return '已中标'
+			} else if (type == '2') {
+				return '未中标'
 			}
 		}
+	},
+	methods: {
+		Tospot(link) {
+			if (link) {
+				this.$router.push({name: link})
+			}
+		},
+		showTotal(total) {
+			return `全部 ${total} 条`;
+		},
+		reloadPage() {
+			location.reload()
+		},
+		getIcon(items) {
+			if (this.$store.state.memberToken) {
+				if (items.statusType == '1' || items.statusType == '2') {
+					if (items.depositNum > 0) {
+						return 'auction_over_01'
+					}
+				} else {
+					if (items.totalPlanNum > 0) {
+						return 'auction_over_02'
+					} else {
+						if (items.depositNum > 0) {
+							return 'auction_over_03'
+						}
+					}
+				}
+			}
+		},
+		handleStatusTypeOption(type) {
+			this.statusType = type
+			location.href = '/bidders?page=' + this.current_page + '&statusType=' + this.statusType + "&planType=" + this.planType
+		},
+		handlePlanTypeOption(type) {
+			this.planType = type
+			location.href = '/bidders?page=' + this.current_page + '&statusType=' + this.statusType + "&planType=" + this.planType
+		},
+
+		async BidersAdd(items, index) {
+			let params = {
+				auctionIds: items.id
+			}
+			const res = await sendCurl(this, server.api.Auction.getfollow, params, false)
+			if (!res.data.errorCode && res.data) {
+				this.$store.dispatch('bidders/getAuctionList', {
+					current_page: this.current_page || 1,
+					page_size: 10,
+					product_type: 1
+				})
+			}
+
+		},
+		async Bidersdelete(items, index) {
+			let params = {
+				auctionId: items.id
+			}
+			const res = await sendCurl(this, server.api.Auction.cancelfollow, params, false)
+			if (!res.data.errorCode && res.data) {
+				this.$store.dispatch('bidders/getAuctionList', {
+					current_page: this.current_page || 1,
+					page_size: 10,
+					product_type: 1
+				})
+			}
+
+		},
+		async SourceData() {
+			let params = {
+				catId: 8
+			}
+			const res = await sendCurl(this, server.api.information.getArticleList, params, false)
+			this.aclist = res.data.items
+		},
+		//跳转详情页
+		BidersDetail(id) {
+
+			if (this.$store.state.memberToken) {
+				location.href = '/bidders/detail/' + id
+			} else {
+				this.$Modal.confirm({
+					title: '提示',
+					content: '<p>请登录后参与竞拍！</p>',
+					okText: '去登录',
+					onOk: () => {
+						location.href = '/login'
+					}
+				});
+			}
+
+		}
+	},
+	watch: {
+		'$route'(to, from) {
+			this.$router.go(0);
+		}
 	}
+}
 </script>
 
 <style lang="less">
