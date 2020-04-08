2c2273c21a8e322454164066f9d21e943877e848
open818 <270608543@qq.com>
2020-04-08 19:07:05 +0800

    no message

---
+++ pages/fdd/selfSignReturn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="body">
+  </div>
+</template>
+
+<script>
+import server from '../../config/api'
+import {sendHttp} from '../../api/common'
+
+export default {
+  name: 'selfSignReturn',
+  data() {
+    return {}
+  },
+  methods: {
+    verifyReturn() {
+      let transaction_id = this.$route.params.transaction_id
+      let result_code = this.$route.params.result_code
+      let result_desc = this.$route.params.result_desc
+      let download_url = this.$route.params.download_url
+      let viewpdf_url = this.$route.params.viewpdf_url
+      let timestamp = this.$route.params.timestamp
+      let msg_digest = this.$route.params.msg_digest
+      sendHttp(this, true, server.api.seal.selfSignReturnUrl, {'transaction_id':transaction_id,'result_code':result_code,'result_desc':result_desc,'download_url':download_url,'viewpdf_url':viewpdf_url,'timestamp':timestamp,'msg_digest':msg_digest}).then(response => {
+        if (response.status === 200) {
+          window.close()
+        }
+      })
+    },
+  },
+  mounted(){
+    this.verifyReturn()
+  }
+}
+</script>
+
---
+++ pages/fdd/verifyReturn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="body">
+  </div>
+</template>
+
+<script>
+import server from '../../config/api'
+import {sendHttp} from '../../api/common'
+
+export default {
+  name: 'verifyReturn',
+  data() {
+    return {}
+  },
+  methods: {
+    verifyReturn() {
+      let companyName = this.$route.params.companyName
+      let transactionNo = this.$route.params.transactionNo
+      let authenticationType = this.$route.params.authenticationType
+      let status = this.$route.params.status
+      let sign = this.$route.params.sign
+      sendHttp(this, true, server.api.seal.verifyReturnUrl, {'companyName':companyName,'transactionNo':transactionNo,'status':status,'sign':sign}).then(response => {
+        if (response.status === 200) {
+          window.close()
+        }
+      })
+    },
+  },
+  mounted(){
+    this.verifyReturn()
+  }
+}
+</script>
+
